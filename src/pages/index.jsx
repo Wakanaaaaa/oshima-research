1bb2ee223408e65fb4e7354574959383dab948f3
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2024-08-23 16:06:08 +0900

    firebaseから中身取得できるようにした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,34 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { collection } from "firebase/firestore";
 
 export default function Word1() {
   const word_1 = ["昨日", "自転車"];
+
+  useEffect(() => {
+    const fetchAllDocuments = async () => {
+      try {
+        const subcollectionRef = collection(db, '4Wwords', "1", 'episodes');
+        const subcollectionSnapshot = await getDocs(subcollectionRef);
+    
+        // ドキュメントのデータを配列に変換
+        const documents = subcollectionSnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+    
+        // 取得したドキュメントを返す
+        console.log(documents);
+      } catch (error) {
+        console.error("Error fetching documents: ", error);
+        throw error;
+      }
+    };
+    
+    fetchAllDocuments();
+  }, []);
 
   return (
     <div>
