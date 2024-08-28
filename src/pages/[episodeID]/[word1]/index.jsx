4e750d169663a18852a8a16283327e6f22671d79
Wakanaaaaa <126571494+Wakanaaaaa@users.noreply.github.com>
2024-08-29 01:32:23 +0900

    被験者IDの設定と、戻るボタンの追加

--- src/pages/[episodeID]/[word1]/index.jsx
+++ src/pages/[episodeID]/[word1]/index.jsx
@@ -1,24 +1,29 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "../firebase";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "@/firebase";
 import { SUBJECT_ID } from "@/subjectID";
 
 export default function Word1() {
+  const router = useRouter();
+  const { episodeID, word1 } = router.query;
   const [keywords, setKeywords] = useState([]); // 空の配列を用意(ステート管理)
 
-  // const word_1 = ["昨日", "自転車"];
-
   useEffect(() => {
-    const fetchAllDocuments = async () => {
+    const fetchDocumentsForEpisode1 = async () => {
       try {
+        // 被験者IDが1のドキュメントのみを取得
         const subcollectionRef = collection(
           db,
           "4Wwords",
           SUBJECT_ID,
           "episodes"
         );
-        const subcollectionSnapshot = await getDocs(subcollectionRef);
+
+        const q = query(subcollectionRef, where("__name__", "==", episodeID)); // "__name__"はFirestoreでドキュメントIDを示します
+        const subcollectionSnapshot = await getDocs(q);
+        console.log("ドキュメントID:", subcollectionSnapshot);
 
         // フィールドを一つの配列にまとめる
         const allFieldsArray = [];
@@ -26,11 +31,13 @@ export default function Word1() {
         //Firestoreから取得したデータを一時的に格納し、後でステートに設定するための作業用配列
         subcollectionSnapshot.forEach((doc) => {
           const data = doc.data();
-          const docID = doc.id; // ドキュメントIDを取得
-          // console.log(doc.id);
+          const docID = doc.id;
+
           // ドキュメントのデータを配列に追加
           for (const [key, value] of Object.entries(data)) {
-            allFieldsArray.push({ key, value, episodeID: docID });
+            if (!word1 || value !== word1) {
+              allFieldsArray.push({ key, value, episodeID: docID });
+            }
           }
         });
 
@@ -54,30 +61,32 @@ export default function Word1() {
       }
     };
 
-    fetchAllDocuments();
-  }, []);
-
-  // keywordsの内容をコンソールに出力
-  useEffect(() => {
-    console.log("Keywordsのstate:", keywords);
-  }, [keywords]);
-
-  console.log(keywords);
+    fetchDocumentsForEpisode1();
+  }, [episodeID, word1]);
 
   return (
     <div>
-      {/* keywordsの内容を画面に表示 */}
       <h3>firebaseからのキーワード:</h3>
+      <h3>選択した単語：{word1}</h3>
       <ul>
         {keywords.map((item, index) => (
           <li key={index}>
-            <Link href={`/${item.episodeID}/${item.value}`}>
-              {item.key}: {item.value}
+            <Link
+              href={{
+                pathname: `/${item.episodeID}/${word1}`,
+                query: { word1: item.value },
+              }}
+            >
+              <button>
+                {item.key}: {item.value}
+              </button>
             </Link>
           </li>
         ))}
       </ul>
-
+      <Link href="/">
+        <button>戻る</button>
+      </Link>
     </div>
   );
 }
---
+++ src/subjectID.js
@@ -0,0 +1,1 @@
+export const SUBJECT_ID = "0122"; // 被験者IDを定義
